e3a7dd5ee4d05059d57ab215772cd70e6cb10508
Brandon Clark <bclarksoftware@gmail.com>
2015-04-16 15:54:06 -0700

    removed program.h and program.cpp

--- Lab1Template/CMakeFiles/lab1.dir/DependInfo.cmake
+++ Lab1Template/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -1,38 +1,37 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/dependencies/glad/src/glad.c" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/dependencies/glad/src/glad.c.o"
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/dependencies/glad/src/glad.c" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/dependencies/glad/src/glad.c.o"
   )
-set(CMAKE_C_COMPILER_ID "Clang")
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/Camera.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Camera.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/GLSL.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/GLSL.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/MatrixStack.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/MatrixStack.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/Program.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Program.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/RenderingHelper.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/RenderingHelper.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/Shape.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Shape.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/Terrain.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Terrain.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/TextureLoader.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/TextureLoader.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/Wall.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Wall.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/main.cpp" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/main.cpp.o"
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/source/tiny_obj_loader.cc" "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/tiny_obj_loader.cc.o"
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/Camera.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Camera.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/GLSL.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/GLSL.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/MatrixStack.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/MatrixStack.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/RenderingHelper.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/RenderingHelper.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/Shape.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Shape.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/Terrain.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Terrain.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/TextureLoader.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/TextureLoader.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/Wall.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/Wall.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/main.cpp" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/main.cpp.o"
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/source/tiny_obj_loader.cc" "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/CMakeFiles/lab1.dir/source/tiny_obj_loader.cc.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/Users/squirrel_matrix/Desktop/CPE476Lab1/Lab1Template/dependencies/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gsuspy/cpe476/CPE476Lab1/Lab1Template/dependencies/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
   )
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "dependencies/glfw/include"
   "dependencies/glad/include"
   )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- Lab1Template/CMakeFiles/lab1.dir/cmake_clean.cmake
+++ Lab1Template/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -1,11 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/lab1.dir/source/main.cpp.o"
   "CMakeFiles/lab1.dir/source/GLSL.cpp.o"
   "CMakeFiles/lab1.dir/source/tiny_obj_loader.cc.o"
   "CMakeFiles/lab1.dir/source/RenderingHelper.cpp.o"
   "CMakeFiles/lab1.dir/source/Camera.cpp.o"
   "CMakeFiles/lab1.dir/source/MatrixStack.cpp.o"
-  "CMakeFiles/lab1.dir/source/Program.cpp.o"
   "CMakeFiles/lab1.dir/source/Shape.cpp.o"
   "CMakeFiles/lab1.dir/source/Terrain.cpp.o"
   "CMakeFiles/lab1.dir/source/TextureLoader.cpp.o"
@@ -16,6 +15,6 @@ file(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
